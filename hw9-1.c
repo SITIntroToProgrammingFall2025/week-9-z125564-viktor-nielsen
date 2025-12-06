84f337f82c8cfa6e86328d91249509176241b31f
z125564-viktor-nielsen <z125564@shibaura-it.ac.jp>
2025-12-07 01:04:24 +0900

    Add files via upload

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main()
+{   int matrix[2][2];
+    int i, j, ad, bc, total; 
+    for (i = 0; i < 2 ; i++)
+    {   
+        for(j = 0; j < 2; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    
+    }
+    ad = matrix[0][0] * matrix[1][1];
+    bc = matrix[1][0] * matrix[0][1];
+    total = ad - bc;
+    printf("You entered\n");
+    for (i = 0; i < 2 ; i++)
+    {   
+        for(j = 0; j < 2; j++)
+        {
+            printf("%d ", matrix[i][j]);
+
+        }
+        printf("\n");
+    
+    }
+    printf("%d", total);
+}
