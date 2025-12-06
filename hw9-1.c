e8de79bdf6ba15559efd14787272d9443efce34a
z125564-viktor-nielsen <z125564@shibaura-it.ac.jp>
2025-12-07 01:21:50 +0900

    Add files via upload

--- hw9-1.c
+++ hw9-1.c
@@ -14,12 +14,11 @@ int main()
     ad = matrix[0][0] * matrix[1][1];
     bc = matrix[1][0] * matrix[0][1];
     total = ad - bc;
-    printf("You entered\n");
     for (i = 0; i < 2 ; i++)
     {   
         for(j = 0; j < 2; j++)
         {
-            printf("%d ", matrix[i][j]);
+            printf("Determinant is %d.000000 ", matrix[i][j]);
 
         }
         printf("\n");
