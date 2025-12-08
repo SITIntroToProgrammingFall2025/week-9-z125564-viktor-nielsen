8cc547214d8df18a5b719a3b67d1636abd3104d3
z125564-viktor-nielsen <z125564@shibaura-it.ac.jp>
2025-12-08 18:26:45 +0900

    Add files via upload

--- hw9-4.c
+++ hw9-4.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main(void)
+{   int rgb[2];
+    int r, g, b;
+    double r_first_digit;
+
+    for (int i = 0; i < 3; i++)
+    {
+        scanf("%d", &rgb[i]);
+    }
+    printf("The hex code is #%x%x%x", rgb[0], rgb[1], rgb[2]);
+}
