b3a0c5f8723d33df5fc4175e0a262ce9d2fddf3a
z125564-viktor-nielsen <z125564@shibaura-it.ac.jp>
2025-12-08 20:05:07 +0900

    Add files via upload

--- hw9-3.c
+++ hw9-3.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int i, j, k;
+    int A[2][3];   
+    int B[3][2];   
+    int C[2][2];   
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 2; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            C[i][j] = 0;
+            for (k = 0; k < 3; k++) {       
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
