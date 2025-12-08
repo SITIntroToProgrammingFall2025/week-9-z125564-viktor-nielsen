36e67e64158881f2ec81816e6ffe871d7bb58289
z125564-viktor-nielsen <z125564@shibaura-it.ac.jp>
2025-12-08 21:12:38 +0900

    Add files via upload

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int i, j, temp;
+    int array[5];
+
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    for (i = 0; i < 5 - 1; i++) {          
+        for (j = 0; j < 5 - 1 - i; j++) {  
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    for (i = 0; i < 5; i++) {
+        printf("%d ", array[i]);
+    }
+
+    return 0;
+}
