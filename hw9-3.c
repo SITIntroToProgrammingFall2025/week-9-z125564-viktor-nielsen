e402ae06d57f6d79c0652533d321904087add3d4
z125564-viktor-nielsen <z125564@shibaura-it.ac.jp>
2025-12-08 20:13:02 +0900

    Add files via upload

--- hw9-3.c
+++ hw9-3.c
@@ -3,36 +3,55 @@
 int main(void)
 {
     int i, j, k;
-    int A[2][3];   
-    int B[3][2];   
-    int C[2][2];   
+    double A[2][3];   
+    double B[3][2];   
+    double C[2][2];   
 
+  
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 3; j++) {
-            scanf("%d", &A[i][j]);
+            scanf("%lf", &A[i][j]);
         }
     }
 
 
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 2; j++) {
-            scanf("%d", &B[i][j]);
+            scanf("%lf", &B[i][j]);
         }
+    }
+
+    printf("The first matrix you entered is\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%g ", A[i][j]);  
+        }
+        printf("\n");
+    }
+
+
+    printf("The second matrix you entered is\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%g ", B[i][j]);
+        }
+        printf("\n");
     }
 
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 2; j++) {
-            C[i][j] = 0;
-            for (k = 0; k < 3; k++) {       
+            C[i][j] = 0.0;
+            for (k = 0; k < 3; k++) {        
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
 
-
+    
+    printf("The multiplication product of matrix A and matrix B:\n");
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 2; j++) {
-            printf("%d ", C[i][j]);
+            printf("%g ", C[i][j]);          
         }
         printf("\n");
     }
